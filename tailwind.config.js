d6b246d99018bde2bf8aa6b7c9dbc3580052e3a5
Kiba Sergey <serhiikiba@gmail.com>
2024-06-14 22:09:25 +0200

    started header styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    screens: {
+      lg: {'max': '1999.99px'},
+      md: {'max': '991.99'},
+      sm: {'max': '767.99'},
+      xs: {'max': '479.99'},
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Monserrat", "sans-serif"]
+      },
+      backgroundImage: {
+        heroGradient:
+            "linear-gradient(94.59deg, #4923B4 2.39%, #E878CF 97.66%)"
+      },
+      colors: {
+        blueviolet: "#5027B5",
+        lightgray: "#666768",
+      }
+    },
   },
   plugins: [],
 }
