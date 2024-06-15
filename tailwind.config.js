e1462d304b76c0de22e8fb4e031bb1dc67cff408
Kiba Sergey <serhiikiba@gmail.com>
2024-06-15 18:45:03 +0200

    finished header and downloaded icons and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,10 @@ export default {
   ],
   theme: {
     screens: {
-      lg: {'max': '1999.99px'},
-      md: {'max': '991.99'},
-      sm: {'max': '767.99'},
-      xs: {'max': '479.99'},
+      lg: {'max': '1199.99px'},
+      md: {'max': '991.99px'},
+      sm: {'max': '767.99px'},
+      xs: {'max': '479.99px'},
     },
     extend: {
       fontFamily: {
